a36915058716eb59b95552778967976ac960d2e3
RogerNogue <rogernogueb@gmail.com>
2020-03-05 22:24:04 +0100

    Added a new class container of menu buttons

--- SDLTest-master/SDLGameTest/ModuleMainMenu.cpp
+++ SDLTest-master/SDLGameTest/ModuleMainMenu.cpp
@@ -1,5 +1,15 @@
 #include "ModuleMainMenu.h"
 
+//sdl includes
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+#include <SDL.h>
+#include <SDL_image.h>
+#elif defined(__APPLE__)
+#include <SDL2/SDL.h>
+#include <SDL2_image/SDL_image.h>
+#endif
+
+//////////Module Main Menu//////////
 
 ModuleMainMenu::ModuleMainMenu()
 {
@@ -7,6 +17,58 @@ ModuleMainMenu::ModuleMainMenu()
 }
 
 ModuleMainMenu::~ModuleMainMenu()
+{
+
+}
+
+bool ModuleMainMenu::Init()
+{
+	return true;
+}
+
+update_status ModuleMainMenu::Update()
+{
+	return UPDATE_CONTINUE;
+}
+
+
+bool ModuleMainMenu::CleanUp()
+{
+	return true;
+}
+
+
+//////////Menu Text class//////////
+
+MenuText::MenuText(int x, int y, int h, int w, char* text)
+{
+	this->text = text;
+	textRect = new SDL_Rect;
+	textRect->x = x; textRect->y = y;
+	textRect->w = w; textRect->h = h;
+}
+
+MenuText::~MenuText()
+{
+	SAFE_RELEASE(textRect);
+}
+
+SDL_Rect* MenuText::GetRect()const
+{
+	return textRect;
+}
+
+void MenuText::SetRect(int x, int y, int h, int w)
+{
+
+}
+
+std::string MenuText::GetText()const
+{
+	return text;
+}
+
+void MenuText::SetText(const char* text)
 {
 
 }
--- SDLTest-master/SDLGameTest/ModuleMainMenu.h
+++ SDLTest-master/SDLGameTest/ModuleMainMenu.h
@@ -2,6 +2,35 @@
 #define _MODULEMAINMENU_
 
 #include "Module.h"
+#include <string>
+
+class SDL_Rect;
+
+//class that contains info about a text
+class MenuText final
+{
+public:
+	MenuText(int x, int y, int w, int h, char* text);
+
+	~MenuText();
+
+	SDL_Rect* GetRect()const;
+
+	void SetRect(int x, int y, int h, int w);
+
+	std::string GetText()const;
+
+	void SetText(const char* text);
+
+public://variables
+	bool highlighted = false;
+
+private://variables
+	SDL_Rect* textRect = nullptr;
+	std::string text = nullptr;
+
+};
+
 class ModuleMainMenu final:
 	public Module
 {
@@ -10,6 +39,14 @@ public:
 
 	~ModuleMainMenu();
 
+	bool Init()override;
+
+	update_status Update() override;
+
+	bool CleanUp()override;
+
+public://variables
+	MenuText playButton;
 };
 
 #endif
